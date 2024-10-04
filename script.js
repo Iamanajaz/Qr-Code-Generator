5b0daffdd30e13b0a343c5dabd763a40d3927e98
Aman Ajaz <139737746+Iamanajaz@users.noreply.github.com>
2024-10-04 15:28:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// function go(){
+    // var txtdata = document.getElementById("data").value;
+    // document.getElementById("qrimg").src="https://en.wikipedia.org/static/images/mobile/copyright/wikipedia-wordmark-en.svg";
+    // alert(txtdata);
+    // 
+// };      
+ 
+// let qrbox = document.getElementById("imgbox");
+// let qrimg = document.getElementById("qrimg");
+// let qrtext = document.getElementById("data");
+// let ze = document.getElementById("size");
+
+
 function go(){
-console.log("aman");
-};
+    document.getElementById("qrimg").src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+data.value;
+    console.log(data.value);
+    // console.log(size.value);
+}
+
