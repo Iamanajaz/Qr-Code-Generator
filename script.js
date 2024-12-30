57b409252b345d133be67b9919e3dac172367616
Aman Ajaz <139737746+Iamanajaz@users.noreply.github.com>
2024-12-30 11:08:28 +0530

    readme file added

--- script.js
+++ script.js
@@ -11,13 +11,28 @@
 // let ze = document.getElementById("size");
 
 
-function go(){
+// function go(){
     
-    let dataval=data.value;
-    if(dataval != null ){
-       document.getElementById("qrimg").src="https://api.qrserver.com/v1/create-qr-code/?size="+size.value+"&data="+dataval;
-        console.log("Input text:",dataval);
-        console.log("Size of Qr image:",size.value); 
-    }
+//     let dataval=data.value;
+//     if(dataval != null ){
+//        document.getElementById("qrimg").src="https://api.qrserver.com/v1/create-qr-code/?size="+size.value+"&data="+dataval;
+//         console.log("Input text:",dataval);
+//         console.log("Size of Qr image:",size.value); 
+//     }
   
+// }
+
+function go() {
+    const data = document.getElementById("data");
+    const size = document.getElementById("size");
+    const qrimg = document.getElementById("qrimg");
+    
+    const dataval = data.value.trim();
+    if (dataval) {
+        qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=" + size.value + "&data=" + encodeURIComponent(dataval);
+        console.log("Input text:", dataval);
+        console.log("Size of QR image:", size.value);
+    } else {
+        alert("Please enter text to generate a QR code!");
+    }
 }
