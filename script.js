aa191b9495e8a04e57e71b8919faf9694fa87edd
Aman Ajaz <amanajaz990@gmail.com>
2026-04-26 23:17:39 +0530

    reset button added

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-function go() {
-    const data = document.getElementById("data");
-    const size = document.getElementById("size");
-    const qrimg = document.getElementById("qrimg");
+// let qrbox = document.getElementById("imgbox");
+// let qrimg = document.getElementById("qrimg");
+// let qrtext = document.getElementById("data");
+// let ze = document.getElementById("size");
+
+
+// function go(){
+//     document.getElementById("qrimg").src="https://api.qrserver.com/v1/create-qr-code/?size="+size.value+"&data="+data.value;
+//     console.log("Input text:",data.value);
+//     console.log("Size of Qr image:",size.value);
+// }
+
+
+
+function go(){
     
-    const dataval = data.value.trim();
-    if (dataval) {
-        qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=" + size.value + "&data=" + encodeURIComponent(dataval);
-        console.log("Input text:", dataval);
-        console.log("Size of QR image:", size.value);
-    } else {
-        alert("Please enter text to generate a QR code!");
+    let dataval=data.value;
+    if(dataval != NaN && dataval.trim() != ""){
+       document.getElementById("qrimg").src="https://api.qrserver.com/v1/create-qr-code/?size="+size.value+"&data="+dataval;
+        console.log("Input text:",dataval);
+        console.log("Size of Qr image:",size.value); 
     }
+    else{
+        alert("Text box is empty");
+    }
+}
+
+function reset(){
+    document.getElementById("qrimg").src="";
+    data.value="";
+    size.value="150x150";
 }
